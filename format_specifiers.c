a7abeb7048e12bcc436237486ba6ebfd6d4d3571
Paintballskaguy <Johnstournament@gmail.com>
2024-06-27 19:47:08 -0500

    created the % handler... checking

--- format_specifiers.c
+++ format_specifiers.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
 /**
- * 
+ * Checking for the s, c and %.
  */
 
+/**handling the % */
+int handle_char(va_list args)
+{
+	char c = va_arg(args, int); /*retrieves the character argument*/
+	_putchar(c); /*prints the character*/
+	return(1); /* returns the number of printed characters*/
+}
 
+/** handling the string s func */
