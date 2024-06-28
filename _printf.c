73f206167778c7b018c19037e550cfd1fbb466e2
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-27 20:12:15 -0500

    Reformatted the beginning a bit. Began first while loop

--- _printf.c
+++ _printf.c
@@ -2,13 +2,17 @@
 /**
  * my_printf - Im not sure how much clearer the title of the program could be.
  * It prints things.
- * 
+ *
  * @format: The format string
- * 
- * Return: The number of characters printed. 
+ *
+ * Return: The number of characters printed.
  */
 int my_printf(const char *format, ...)
 {
 	va_list args;
+	int count = 0;
+	int i = 0;
+
 	va_start(args, format);
-}
+	while (format[i] != '\0')
+	{}
