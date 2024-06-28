d21a020b7c8fbd37e92b0de69e772e68f54810e0
Paintballskaguy <Johnstournament@gmail.com>
2024-06-27 20:23:50 -0500

    finished the handle_string - pending testing

--- format_specifiers.c
+++ format_specifiers.c
@@ -13,3 +13,25 @@ int handle_char(va_list args)
 }
 
 /** handling the string s func */
+
+int handle_string(va_list args)
+{
+	char *str = va_arg(args char *); /* gets the string argument*/
+	int i; /*loop counter*/
+
+	if (str == NULL)
+		str = "(null)"; /*handles the null string*/
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]); /* print the character at the array address*/
+
+	return (i); /*returns the number of printed characters*/
+}
+
+/**
+ * Handling the c .char stuff
+ */
+
+int handle_char(va_list args)
+{
+	
+}
