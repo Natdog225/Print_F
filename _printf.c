0126711ab393c403bc6fa4c787d1cee18c49e93a
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-27 19:45:14 -0500

    Setup printf with basic stuff, betty comments, prototype and beginning initializations.

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+/**
+ * my_printf - Im not sure how much clearer the title of the program could be.
+ * It prints things.
+ * 
+ * @format: The format string
+ * 
+ * Return: The number of characters printed. 
+ */
+int my_printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+}
