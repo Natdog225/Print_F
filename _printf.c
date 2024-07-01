6a26224f10cd9b24e00124bfbce114b18faa866b
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-30 19:33:39 -0500

    Ideally fixed compile errors

--- _printf.c
+++ _printf.c
@@ -12,43 +12,42 @@ int my_printf(const char *format, ...)
 	va_list args;
 	int count = 0;
 	int i = 0;
-	long int num;
-	int num_temp;
-	char digit;
+	
+    va_start(args, format);
+    while (format[i] != '\0')
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == 'c')
+            {
+                count += _putchar(va_arg(args, int)); 
+            }
+            else if (format[i] == 's')
+            {
+                char *str = va_arg(args, char *);
+                while (*str != '\0')
+                {
+                    count += _putchar(*str++);
+                }
+            }
+            else if (format[i] == '%')
+            {
+                count += _putchar('%');
+            }
+            else
+            {
+                count += _putchar('%');
+                count += _putchar(format[i]);
+            }
+        }
+        else
+        {
+            count += _putchar(format[i]);
+        }
+        i++;
+    }
 
-	va_start(args, format);
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == 'c')
-			{
-				count += _putchar(va_arg(args, int));
-			}
-			else if (format[i] == 's')
-			{
-				char *str = va_arg(args, char *);
-				while (*str != '\0')
-				{
-					count += _putchar(*str++);
-				}
-				else if (format[i] == '%')
-				{
-					count += _putchar('%');
-				}
-				else
-				{
-					count += _putchar('%');
-					count += _putchar(format[i]);
-				}
-				else
-				{
-					count += _putchar(format[i]);
-				}
-				i++;
-			}
-			va_end(args);
-			return (count);
-		}
-	}
+    va_end(args);
+    return (count);
+}
