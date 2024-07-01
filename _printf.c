52a591c18773832712aab72fbf4381648d6ec25e
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-30 20:17:15 -0500

    added check so it does not move on to chars incorrectly

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1] != '\0');
 		{
 			i++;
 			if (format[i] == 'c')
