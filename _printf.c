1223b5f24a588b4cbc65b5a38a94505dcbf71979
Paintballskaguy <Johnstournament@gmail.com>
2024-06-30 21:50:44 -0500

    updated printf with a && on % statement

--- _printf.c
+++ _printf.c
@@ -21,7 +21,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1] != '\0')
 		{
 			i++;
 			if (format[i] == 'c')
